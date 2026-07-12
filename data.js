98dd386a53516b6822571698907f56455f5db1da
hazelhimelstein <hazelhimelstein@gmail.com>
2026-07-12 16:20:28 -0700

    Update data.js

--- data.js
+++ data.js
@@ -2,7 +2,9 @@ const people = [
   { id: 1, label: "Hazel" },
   { id: 2, label: "Brooke" },
   { id: 3, label: "Alex" },
-  { id: 4, label: "Liza" }
+  { id: 4, label: "Liza" },
+  { id: 5, label: "Neha" },
+  { id: 4, label: "Josie" }
 ];
 
 const relationships = [
@@ -17,12 +19,36 @@ const relationships = [
     to: 4,
     color: "hotpink",
     dashes: true,
-    label: "Situationship"
+    label: "Hookup"
   },
   {
     from: 3,
     to: 4,
     color: "red",
     label: "Roommates"
+  },
+  {
+    from: 2,
+    to: 3,
+    color: "red",
+    label: "Hookup"
+  },
+  {
+    from: 4,
+    to: 5,
+    color: "red",
+    label: "Hookup"
+  },
+  {
+    from: 2,
+    to: 3,
+    color: "red",
+    label: "Hookup"
+  },
+  {
+    from: 1,
+    to: 5,
+    color: "red",
+    label: "Hookup"
   }
 ];
